c4d6140cc53d9f1581f0434d18a07ba86e8b6554
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-06 20:10:52 -0400

    Delete html files from repo.

--- Sources/Site/main.swift
+++ Sources/Site/main.swift
@@ -21,47 +21,13 @@ let sitemap: [String: any View] = [
 
 for (path, view) in sitemap {
   let output = try "<!DOCTYPE html>\n" + renderHTML(view)
+  let fileURL = siteURL.appending(path: path)
+  let folderURL = fileURL.deletingLastPathComponent()
+  if !FileManager.default.fileExists(atPath: folderURL.path(percentEncoded: false)) {
+    try FileManager.default.createDirectory(at: folderURL, withIntermediateDirectories: true)
+  }
   try output.write(to: siteURL.appending(path: path), atomically: true, encoding: .utf8)
 }
-
-try """
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.html"],
-  theme: {
-    extend: {
-      boxShadow: {
-        'puck': '0 0 4px 0 #0003,0 2px 0 0 #0000001a' 
-      }
-    },
-    fontFamily: {
-      'rounded': ['ui-rounded', '-apple-system', 'system-ui', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif']
-    },
-    container: {
-      center: true
-    },
-    screens: {
-      'sm': '375px',
-      'md': '900px',
-    }
-  },
-  plugins: [],
-}
-""".write(
-  to: projectRootURL.appending(path: "tailwind.config.js"),
-  atomically: true,
-  encoding: .utf8
-)
-
-try """
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-""".write(
-  to: sourceURL.appending(path: "tailwind.css"),
-  atomically: true,
-  encoding: .utf8
-)
 
 let process = Process()
 let pipe = Pipe()
@@ -83,35 +49,3 @@ guard let standardOutput = String(data: data, encoding: .utf8) else {
 }
 
 print(standardOutput)
-
-
-
-//module.exports = {
-//  content: [
-//    "./index.html",
-//    "./privacy-policy/*.html",
-//    "./scanning/*.html",
-//    "./scanning/extended-pids/*.html",
-//    "./scanning/repo-status/*.html",
-//    "./shortcuts/*.html"
-//  ],
-//  darkMode: 'media',
-//  theme: {
-//    extend: {
-//      colors: {
-//        'sidecar-gray': '#262625',
-//      }
-//    },
-//    fontFamily: {
-//      'rounded': ['ui-rounded', '-apple-system', 'system-ui', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif']
-//    },
-//    container: {
-//      center: true
-//    },
-//    screens: {
-//      'sm': '375px',
-//      'md': '900px',
-//    }
-//  },
-//  plugins: [],
-//}
